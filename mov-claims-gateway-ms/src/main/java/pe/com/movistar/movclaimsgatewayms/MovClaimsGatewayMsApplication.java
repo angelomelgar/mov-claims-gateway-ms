d24cb4370910ed8f8aa290dc0e43190a7cfc8144
angelomelgar <amelgargozzer@gmail.com>
2021-05-25 16:04:03 -0500

    Add files via upload

---
+++ mov-claims-gateway-ms/src/main/java/pe/com/movistar/movclaimsgatewayms/MovClaimsGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package pe.com.movistar.movclaimsgatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovClaimsGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovClaimsGatewayMsApplication.class, args);
+	}
+
+}
---
+++ mov-claims-gateway-ms/src/test/java/pe/com/movistar/movclaimsgatewayms/MovClaimsGatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.movistar.movclaimsgatewayms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovClaimsGatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
